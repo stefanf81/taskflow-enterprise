24c26b788fb44357d5e987f08b8ee41fb0cbe57d
stefanf81 <stefan_faes@hotmail.com>
2026-07-17 12:33:34 +0200

    chore: disable Angular analytics, enable in-memory scrolling with scroll restoration

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,19 +1,30 @@
 import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import {
   provideRouter,
+  withInMemoryScrolling,
   withPreloading,
   PreloadAllModules,
   withViewTransitions,
 } from '@angular/router';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { authInterceptor } from './auth.interceptor';
 
+import { authInterceptor } from './auth.interceptor';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZonelessChangeDetection(),
-    provideRouter(routes, withPreloading(PreloadAllModules), withViewTransitions()),
-    provideHttpClient(withInterceptors([authInterceptor])),
+
+    provideRouter(
+      routes,
+      withPreloading(PreloadAllModules),
+      withViewTransitions(),
+      withInMemoryScrolling({
+        scrollPositionRestoration: 'enabled',
+        anchorScrolling: 'enabled',
+      }),
+    ),
+
+    provideHttpClient(withFetch(), withInterceptors([authInterceptor])),
   ],
 };
